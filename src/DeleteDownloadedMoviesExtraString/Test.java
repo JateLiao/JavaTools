e3fcd2069c8b3bbf4649b2b45954eb6b009cd361
Shijie Liao <374682617@qq.com>
2017-01-13 23:13:26 +0800

    first commit all files on 2017/1/13

---
+++ src/DeleteDownloadedMoviesExtraString/Test.java
@@ -0,0 +1,39 @@
+﻿/*
+ * 文件名：Test.java
+ * 版权：Copyright 2007-2016 KOBE Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： Test.java
+ * 修改人：KOBE
+ * 修改时间：2016年9月10日
+ * 修改内容：新增
+ */
+package DeleteDownloadedMoviesExtraString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     KOBE
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("maxMemory:   " + Runtime.getRuntime().maxMemory());
+        System.out.println("totalMemory: " + Runtime.getRuntime().totalMemory());
+        System.out.println("freeMemory:  " + Runtime.getRuntime().freeMemory());
+        
+        // String s = "[电影天堂www.dy2018.net]纵横四海.1024x548.国粤双语.中文字幕.mkv";
+        // System.out.println(s.replaceAll("\\[.*?\\]", ""));
+        Map<String, String> map = new HashMap<>();
+
+        String s = "TDDownload..试试.rmvb";
+        System.out.println(s.replaceAll("TDDownload/./.", ""));
+    }
+}
---
+++ src/RMB2CHN/Test.java
@@ -0,0 +1,27 @@
+﻿/*
+ * 文件名：Test.java
+ * 版权：Copyright 2007-2016 Liao Shijie Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： Test.java
+ * 修改人：KOBE
+ * 修改时间：2016年11月13日
+ * 修改内容：新增
+ */
+package RMB2CHN;
+
+/**
+ * TODO 添加类的一句话简单描述.
+ * <p>
+ * TODO 详细描述
+ * <p>
+ * TODO 示例代码
+ * <pre>
+ * </pre>
+ * 
+ * @author     KOBE
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(RMB2CHN.rmb2CHN("548465.55"));
+    }
+}
