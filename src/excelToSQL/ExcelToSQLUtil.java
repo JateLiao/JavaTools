36d7cb8a2902ffe26513fe8c7f7c25acb35994c7
Shijie Liao <374682617@qq.com>
2017-03-07 22:25:16 +0800

    add the util:excel to sql(insert)

---
+++ src/excelToSQL/ExcelToSQLUtil.java
@@ -0,0 +1,23 @@
+/*
+ * 文件名：ExcelToSQLUtil.java
+ * 版权：Copyright 2007-2017 Liao Shijie Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： ExcelToSQLUtil.java
+ * 修改人：KOBE
+ * 修改时间：2017年3月7日
+ * 修改内容：新增
+ */
+package excelToSQL;
+
+/**
+ * TODO 在excel表格的数据，导出成sql语句.
+ * 
+ * 主要是导出为insert的sql语句.
+ * 
+ * @author     KOBE
+ */
+public class ExcelToSQLUtil {
+
+    public static String toInsertSql() {
+        return "";
+    }
+}
