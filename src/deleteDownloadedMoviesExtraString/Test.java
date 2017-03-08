a18b329952ce1b76f52c863fd4b3c81a51b366c5
JateLiao <374682617@qq.com>
2017-03-08 10:01:35 +0800

    xx

--- src/deleteDownloadedMoviesExtraString/Test.java
+++ src/deleteDownloadedMoviesExtraString/Test.java
@@ -26,7 +26,7 @@ public class Test {
         // String s = "[电影天堂www.dy2018.net]纵横四海.1024x548.国粤双语.中文字幕.mkv";
         // System.out.println(s.replaceAll("\\[.*?\\]", ""));
         Map<String, String> map = new HashMap<>();
-        System.out.println(map.size());
+        System.out.println(map.size()); 
         String s = "TDDownload..试试.rmvb";
         System.out.println(s.replaceAll("TDDownload/./.", ""));
     }
