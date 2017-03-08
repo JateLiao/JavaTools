d9410eb69f77ff3ed82170235982535e74e23ae4
JateLiao <374682617@qq.com>
2017-03-08 09:57:16 +0800

    去掉unused提示

--- src/deleteDownloadedMoviesExtraString/Test.java
+++ src/deleteDownloadedMoviesExtraString/Test.java
@@ -13,12 +13,6 @@ import java.util.Map;
 
 /**
  * TODO 添加类的一句话简单描述.
- * <p>
- * TODO 详细描述
- * <p>
- * TODO 示例代码
- * <pre>
- * </pre>
  * 
  * @author     KOBE
  */
@@ -32,7 +26,7 @@ public class Test {
         // String s = "[电影天堂www.dy2018.net]纵横四海.1024x548.国粤双语.中文字幕.mkv";
         // System.out.println(s.replaceAll("\\[.*?\\]", ""));
         Map<String, String> map = new HashMap<>();
-
+        System.out.println(map.size());
         String s = "TDDownload..试试.rmvb";
         System.out.println(s.replaceAll("TDDownload/./.", ""));
     }
