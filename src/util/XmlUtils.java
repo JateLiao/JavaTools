771bf49af9f88addcc7001b6d0bb92e4912d24ad
Shijie Liao <374682617@qq.com>
2017-09-19 23:27:41 +0800

    XmlUtils

---
+++ src/util/XmlUtils.java
@@ -0,0 +1,17 @@
+/*
+ * 文件名：XmlUtils.java
+ * 版权：Copyright 2007-2017 Liao Shijie Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： XmlUtils.java
+ * 修改人：KOBE
+ * 修改时间：2017年9月19日
+ * 修改内容：新增
+ */
+package util;
+
+/**
+ * TODO Java Xml相关常用工具集.
+ * @author     KOBE
+ */
+public class XmlUtils {
+
+}
